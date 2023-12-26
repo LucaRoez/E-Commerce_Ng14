b3fc2560a34e8850f856f520c993c433a70edf0d
Voltaire.Spooner <luca.ezequiel.rodriguez@gmail.com>
2023-12-25 22:30:29 -0300

    carousel and files loading issues resolved.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -9,14 +9,14 @@ export class HomeComponent {
   images: ImageKeeper[] = [
 
     {
-      src: "assets/img/home-carousel-male.png",
+      src: "home-carousel-male.png",
       alt: "menswear kind",
       title: "Minimal Men Style",
       description: "Consectetur enim commodo eiusmod excepteur pariatur amet ad adipisicing dolor. Culpa in ex quis aliqua deserunt dolor fugiat Lorem sunt ex ipsum. Occaecat consequat aliquip exercitation reprehenderit reprehenderit sint."
     },
 
     {
-      src: "assets/img/home-carousel-female.png",
+      src: "home-carousel-female.png",
       alt: "feminine fashion kind",
       title: "Minimal Ladies Style",
       description: "Velit qui est adipisicing excepteur ut ullamco labore pariatur proident. Nulla anim enim aliquip qui voluptate in velit minim commodo qui ad occaecat dolore. Ex proident laborum nisi exercitation aliquip. In laborum labore minim esse incididunt eiusmod exercitation ea sint. Lorem esse excepteur laborum labore. Ex quis eu veniam adipisicing."
