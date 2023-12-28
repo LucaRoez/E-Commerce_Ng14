0ab6006bd12bea7a64a758c9ffbe90d154e411da
Voltaire.Spooner <luca.ezequiel.rodriguez@gmail.com>
2023-12-27 23:37:16 -0300

    adding flex, and footer finished.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -18,7 +18,7 @@ export class HomeComponent {
       src: "home-carousel-female.png",
       alt: "feminine fashion kind",
       title: "Minimal Ladies Style",
-      description: "Velit qui est adipisicing excepteur ut ullamco labore pariatur proident. Nulla anim enim aliquip qui voluptate in velit minim commodo qui ad occaecat dolore. Ex proident laborum nisi exercitation aliquip. In laborum labore minim esse incididunt eiusmod exercitation ea sint. Lorem esse excepteur laborum labore. Ex quis eu veniam adipisicing."
+      description: "Velit qui est adipisicing excepteur ut ullamco labore pariatur proident. Nulla anim enim aliquip qui voluptate in velit minim commodo qui ad occaecat dolore. Ex proident laborum nisi exercitation aliquip."
     }
   ]
 
