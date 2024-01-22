535494b55ffbe7aff3752c2b2c9f2ca7e12c0716
Voltaire.Spooner <luca.ezequiel.rodriguez@gmail.com>
2024-01-21 22:02:51 -0300

    SQL complex query to start the project is done.

--- E-Commerce.database/InitDatabase/StartProject.sql
+++ E-Commerce.database/InitDatabase/StartProject.sql
@@ -1,7 +1,5 @@
 ﻿create database [E-Commerce Database];
-
 use [E-Commerce Database];
-go;
 
 create table [c.Currencies] (
 	Id tinyint primary key,
@@ -17,11 +15,28 @@ create table [c.Categories] (
 
 create table [c.Gender] (
 	Id tinyint primary key,
-	name varchar(5) not null
+	Name varchar(5) not null
+);
+
+create table [d.Users] (
+	Id int identity(1,1) primary key,
+	UserName varchar(20) not null,
+	FirstName varchar(20),
+	LastName varchar(20),
+	Email varchar(50) not null,
+	Password varchar(250) not null
+);
+
+create table [d.Authors] (
+	Id smallint identity(1,1) primary key,
+	AuthorName varchar(20),
+	Presentation varchar(50),
+	Email varchar(50) not null,
+	Password varchar(250) not null
 );
 
 create table [d.Products] (
-	Id bigint primary key,
+	Id bigint identity(1,1) primary key,
 	Name varchar(50) not null,
 	Description varchar(5000) not null,
 	Price smallmoney,
@@ -37,13 +52,26 @@ create table [d.Products] (
 );
 
 create table [d.Images] (
-	Id bigint primary key
+	Id bigint primary key,
+	Src varchar(250) not null,
+	Alt varchar(50),
+	Width int not null,
+	Heigth int not null
 );
 
 create table [d.Reviews] (
-	Id bigint identity(1,1) primary key
+	Id bigint identity(1,1) primary key,
+	Author varchar(20) not null,
+	AuthorId smallint foreign key references [d.Authors](Id),
+	Body varchar(5000) not null,
+	Rate decimal(2,1)
 );
 
 create table [d.Commentaries] (
-	Id bigint identity(1,1) primary key
+	Id bigint identity(1,1) primary key,
+	UserName varchar(20) not null,
+	Body varchar(5000) not null,
+	IsEdited bit not null,
+	Likes int not null,
+	Dislikes int not null
 );
